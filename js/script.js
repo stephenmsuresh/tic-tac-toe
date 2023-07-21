25bb0fffef43108da729eff8bfb619760b5107c0
Stephen Suresh <stephen.suresh@outlook.com>
2023-07-21 15:46:06 -0400

    change board array to be 1-d instead of 2-d

--- js/script.js
+++ js/script.js
@@ -1,43 +1,18 @@
 const TicTacToeBoard = (() => {
-    let rows = 3;
-    let columns = 3;
+    let board = new Array(9);
 
-    //3x3 array
-    // row 1
-    // row 2
-    // row 3
-    let board = [];
-    for (let i = 0; i < rows; i++) {
-        board[i] = [];
-        for (let j = 0; j < columns; j++) {
-            board[i][j];
-            // change this to ""
+    let fillSpot = (cell, marker) => {
+        if (!board[cell]) {
+            board[cell] = marker
         }
-    }
-
-    let fillSpot = (row, column, marker) => {
-        if (!(row >= 0) ||
-            !(row < 3) ||
-            !(column >= 0) ||
-            !(column < 3) ||
-            (board[row][column])) {
-            return
-        }
-        board[row][column] = marker;
     }
 
     let getBoard = () => {
         return board;
     }
 
-    let getMark = (row, column) => {
-        if (!(row >= 0) ||
-            !(row < 3) ||
-            !(column >= 0) ||
-            !(column < 3)) {
-            return
-        }
-        return board[row][column];
+    let getMark = (cell) => {
+        return board[cell];
     }
 
 
@@ -68,61 +43,48 @@ let GameController = (() => {
     }
 
     let winConditions = [
-        ["01", "02", "03"],
-        ["10", "11", "12"],
-        ["20", "21", "22"],
-        ["00", "10", "20"],
-        ["01", "11", "12"],
-        ["02", "12", "22"],
-        ["00", "11", "22"],
-        ["02", "11", "20"]
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
     ]; //any of these 3 all have to have the same symbol to win
 
     //function that checks win after every move
     let checkWin = () => {
-        winConditions.forEach((winCondition) => {
+        winConditions.forEach((condition) => {
+            let currMark = '';
+            let prevMark = '';
+            let row, col, counter;
+            counter = 0;
             for (let i = 0; i < 3; i++) {
-                let counter = 0;
-                let row, col;
-                for (let j = 0; j < 3; j++) {
-                    let currentMark = ""
-                    let previousMark = ""
-                    [row, col] = winCondition[i][j].split();
-                    currentMark = board.getMark(row, col);
-                    if (counter === 0) { //first instance of mark
-                        counter++;
-                        previousMark = currentMark;
-                    }
-                    else {
-                        if (currentMark === previousMark) {
-                            counter++;
-                            previousMark = currentMark;
-                            if (counter === 3) {
-                                console.log("win");
-                                return `Player ${currentPlayer.player} has won!`; //there is a win,
-                            }
-                        }
-                        else {
-                            break; //break out of j foor loop to go to the next win condition combination
-                        }
-                    }
+                currMark = board.getMark(condition[i]);
+                if (!prevMark) {
+                    prevMark = currMark;
+                    counter++;
                 }
-
+                else if (currMark === prevMark) {
+                    counter++;
+                }
+                else {
+                    break;
+                }
             }
-            if (roundsPlayed < 9) {
-                console.log("no win yet")
-                return false; //end of checking all win conditions
+            if (counter === 3) {
+                return `Player ${currentPlayer.name} won!`
             }
-            console.log("tie");
-            return `Game Over! It's a Tie!`
-        })
+        });
     }
 
-    let playRound = (row, column) => {
-        board.fillSpot(row, column, currentPlayer.mark);
-        console.log(board.getMark(row, column));
+    let playRound = (cell) => {
+        board.fillSpot(cell, currentPlayer.mark);
         let msg = checkWin();
+        console.log(msg);
         if (msg) {
+            console.log(msg);
             return msg;
         }
         else {
@@ -151,13 +113,8 @@ let displayController = (() => {
     function placeMarker(e) {
         let row, col;
         [row, col] = (e.target.id).slice(1).split("");
-        console.log(row);
-        console.log(col);
-        console.log(GameController.board.getMark(+row, +col));
-        let text = GameController.board.getMark(row, col);
-        console.log(text);
-        e.target.textContent = text
         let msg = GameController.playRound(row, col);
-        console.log(msg);
+        let text = GameController.board.getMark(row, col);
+        e.target.textContent = text
     }
 })();
